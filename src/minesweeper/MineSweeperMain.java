6c2b10f6ef34c4dfbd195aa75bc9010580ecc433
ThDilos <2273656532co@gmail.com>
2024-04-06 23:38:43 +0800

    1. A new class "StatusSection" to represent the top section of the game board 2. A new class "CustomTimerAction" to implement the timer label on the StatusSection 3. Implemented Restart function and Timer function 4. As a result, three new variables: 4.1. ActualTimer (CustomTimerAction) 4.2. timer(Timer) (From javax.swing) 4.3. statusSection (StatusStection)
    Note: The layout is kinda messed up, plz adjust it if you have time

--- src/minesweeper/MineSweeperMain.java
+++ src/minesweeper/MineSweeperMain.java
@@ -1,4 +1,5 @@
 package minesweeper;
+
 import java.awt.*;        // Use AWT's Layout Manager
 import java.awt.event.*;
 import javax.swing.*;     // Use Swing's Containers and Components
@@ -14,17 +15,17 @@ public class MineSweeperMain extends JFrame {
 
    // private variables
    GameBoardPanel board = new GameBoardPanel();
-   JButton btnNewGame = new JButton("New Game");
+   CustomTimerAction ActualTimer; // The actual timer recording the parameters 
+   Timer timer; // The internal timer
+   StatusSection statusSection = new StatusSection(); // The top panel
 
    // Constructor to set up all the UI and game components
    public MineSweeperMain() {
       Container cp = this.getContentPane();           // JFrame's content-pane
       cp.setLayout(new BorderLayout()); // in 10x10 GridLayout
 
+      cp.add(statusSection, BorderLayout.NORTH);
       cp.add(board, BorderLayout.CENTER);
-
-      // Add btnNewGame to the south to re-start the game
-      // ......
 
       board.newGame();
 
@@ -32,6 +33,84 @@ public class MineSweeperMain extends JFrame {
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // handle window-close button
       setTitle("Minesweeper");
       setVisible(true);   // show it
+   }
+
+   public class StatusSection extends JPanel { // This is the top section panel added into the minesweeper
+      //Private variables
+      private JButton Restart; // Restart Button
+      private JLabel LabelTimer; // Timer Label
+      
+      public StatusSection() { //Constructor
+         super.setLayout(new GridLayout(1, 3, 250, 1)); //Set up the panel into gridlayout
+         Restart = new JButton("Restart"); //Initialise the Button with text "Restart"
+         super.add(Restart); //Add start to the top panel
+
+         // To achieve the restart function
+         Restart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               board.newGame();
+               ActualTimer.Reset(); 
+               System.out.println("Game Restarted!"); // Output to log
+            }
+         });
+
+         LabelTimer = new JLabel("0s"); //Initialise the Label
+         ActualTimer  = new CustomTimerAction(LabelTimer); //Initialise the ActualTimer, used to record down second, minute, hour
+         timer = new Timer(1000, ActualTimer); // Initialise the timer, and add ActualTimer to the actionPerformed
+         timer.start(); //Start Timer
+         super.add(LabelTimer); // Add the label onto the top panel
+      }
+   }
+
+   public class CustomTimerAction implements ActionListener {
+      //Private variables
+      private int second, minute, hour;
+      private JLabel label;
+
+      @Override //This actionPerformed runs every second
+      public void actionPerformed(ActionEvent e) {
+         second++; //Classic Time manipulation
+         if(second == 60) {
+            second = 0;
+            minute++;
+         }
+         if(minute == 60) {
+            second = 0;
+            minute = 0;
+            hour++;
+         }
+         
+         label.setText(this.toString()); // Set the timer label per second
+
+         if(board.hasLost) {
+            label.setText("You've Lost!"); //Replace the timer with "You've Lost!"
+            timer.stop(); //Stop the internal timer
+         }
+      }
+
+      public CustomTimerAction(JLabel UpdatingLabel) {
+         this.label = UpdatingLabel; // Point to the Timer label to modify
+      }
+
+      // This is used to reset the timer
+      public void Reset() {
+         this.second = 0;
+         this.minute = 0;
+         this.hour = 0; //Reset the 3 parameters
+         board.hasLost = false;
+         timer.restart(); //Restart the 1s internal timer, negligible
+         label.setText("0s"); //Reset Label to 0s
+      }
+
+      public String toString() { //Return in desired Time Format
+         if(hour > 0) // Dumb method for output 00 format. There is definitely a String.format way to do but I'm too lazy to find out ;)
+            return hour + " h " + (minute < 10 ? "0"+ minute : minute) + " min " + (second < 10 ? "0"+ second : second) + " s";
+         else if(minute > 0) 
+            return minute + " min " + (second < 10 ? "0"+ second : second) + " s";
+         else
+            return second + " s";
+      }
    }
 
    // The entry main() method
