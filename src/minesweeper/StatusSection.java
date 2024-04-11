43c89cadfea9180594331762441285ebb8eb8cb2
ThDilos <2273656532co@gmail.com>
2024-04-11 22:24:56 +0800

    A goBack button implemented

--- src/minesweeper/StatusSection.java
+++ src/minesweeper/StatusSection.java
@@ -6,7 +6,7 @@ import javax.swing.*;     // Use Swing's Containers and Components
 
 public class StatusSection extends JPanel { // This is the top section panel added into the minesweeper
     //Private variables
-    private JButton Restart; // Restart Button
+    private JButton restart, goBack; // restart Button & Back to Menu Button
     private JLabel LabelTimer; // Timer Label
     private CustomTimerAction ActualTimer; // The actual timer recording the parameters 
     private Timer timer; // The internal timer
@@ -14,17 +14,21 @@ public class StatusSection extends JPanel { // This is the top section panel add
     
     public StatusSection(MineSweeperMain controlCenter) { //Constructor
         this.controlCenter = controlCenter;
-        super.setLayout(new GridLayout(1, 3, 250, 1)); //Set up the panel into gridlayout
-        Restart = new JButton("Restart"); //Initialise the Button with text "Restart"
-        super.add(Restart); //Add start to the top panel
+        super.setLayout(new GridLayout(1, 3, 50, 1)); //Set up the panel into gridlayout
+        restart = new JButton("Restart"); //Initialise the Button with text "Restart"
+
+        goBack = new JButton("Return");
+
+        super.add(goBack);
+        super.add(restart); //Add start to the top panel
 
         // To achieve the restart function
-        Restart.addActionListener(new ActionListener() {
+        restart.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 controlCenter.getBoard().newGame();
                 resetTimer();
-                System.out.println("Game Restarted!"); // Output to log
+                System.out.println("Game restarted!"); // Output to log
             }
         });
 
@@ -48,6 +52,10 @@ public class StatusSection extends JPanel { // This is the top section panel add
 
     public CustomTimerAction getActualTimer() {
         return this.ActualTimer;
+    }
+
+    public JButton getGoBackButton() {
+        return this.goBack;
     }
 
     public void resetTimer() {
