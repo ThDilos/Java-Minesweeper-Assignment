2c559d6fc54f5729f38417d78beb4d1787011248
ThDilos <2273656532co@gmail.com>
2024-04-15 23:14:24 +0800

    Changed the looks a bit.

--- src/minesweeper/StartMenu.java
+++ src/minesweeper/StartMenu.java
@@ -5,10 +5,11 @@ import java.awt.*;
 
 public class StartMenu extends JPanel { // This is the start menu you see on initialisation
     private JButton start, settings, rankings;
+    private JLabel bigTitle;
     private Dimension ButtonDimension = new Dimension(200, 30);
 
     public StartMenu(MineSweeperMain controlMain) {
-
+        super.setBackground(Color.LIGHT_GRAY);
         super.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
@@ -16,8 +17,14 @@ public class StartMenu extends JPanel { // This is the start menu you see on ini
         gbc.gridy = 0;
         gbc.ipady = 10; // This changes the upper lower padding of individual components
         gbc.anchor = GridBagConstraints.CENTER; // This serves no purpose, but good to have maybe
-        gbc.insets = new Insets(10,0,0,0); // The first int changes the distance between two components, dk about others havn't tried 
+        gbc.insets = new Insets(10,0,10,0); // The first int changes the distance between two components, dk about others havn't tried 
 
+        bigTitle = new JLabel("MINESWEEPER YIPEE");
+        bigTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        bigTitle.setFont(new Font("Monospaced", Font.BOLD, 50));
+        super.add(bigTitle, gbc);
+
+        gbc.gridy = 1;
         start = new JButton("Start");
         start.setPreferredSize(ButtonDimension);
         start.setFont(new Font("Arial", Font.PLAIN, (int)ButtonDimension.getHeight() - 2)); 
@@ -26,11 +33,11 @@ public class StartMenu extends JPanel { // This is the start menu you see on ini
         settings = new JButton("Settings");
         settings.setPreferredSize(new Dimension(200, 30));
         settings.setFont(new Font("Arial", Font.PLAIN, (int)ButtonDimension.getHeight() - 2));
-        gbc.gridy = 1;
+        gbc.gridy = 2;
         super.add(settings, gbc);
 
         rankings = new JButton("Rankings");
-        gbc.gridy = 2;
+        gbc.gridy = 3;
         rankings.setPreferredSize(ButtonDimension);
         rankings.setFont(new Font("Arial", Font.PLAIN, (int)ButtonDimension.getHeight() - 2));
         super.add(rankings, gbc);
