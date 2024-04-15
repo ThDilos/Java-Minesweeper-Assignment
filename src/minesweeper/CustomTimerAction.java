7113a1584410823116081e8d7b335a6a4477b776
ThDilos <2273656532co@gmail.com>
2024-04-15 23:12:53 +0800

    Twisted how score calculation work. You are guaranteed at least 50 score if you win. And you will get the portion of the mineFlagged if you lose

--- src/minesweeper/CustomTimerAction.java
+++ src/minesweeper/CustomTimerAction.java
@@ -63,6 +63,7 @@ public class CustomTimerAction implements ActionListener {
         int timeTaken = second + minute * 60 + hour * 60;
         int difficulty = board.getDifficulty();
         int mineFlagged = board.getMineFlagged();
+        float finalScore;
 
         if (this.board.hasWon()) {
             switch (difficulty) {
@@ -70,37 +71,56 @@ public class CustomTimerAction implements ActionListener {
                 // Easy Difficulty
                 case 0:
                     if(timeTaken < 15)
-                        return 100f;
+                        finalScore =  100f;
                     else if(timeTaken < 120)
-                        return (float)(-(4.0 / 441.0) * (timeTaken - 15) * (timeTaken - 15) + 100); // Powered by ChatGPT, this expression will result in an increasingly decreasing function when x > 15, and reaches 0 when x = 120
+                        finalScore =  (float)(-(2.0 / 441.0) * (timeTaken - 15) * (timeTaken - 15) + 100); // Powered by ChatGPT, this expression will result in an increasingly decreasing function when x > 15, and reaches 50 when x = 120
                     else
-                        return 0.0f;
+                        finalScore =  50.0f;
             
                 //Normal Difficulty
                 case 1:
                     if(timeTaken < 60)
-                        return 100f;
+                        finalScore =  100f;
                     else if(timeTaken < 180)
-                        return (float)(-(1.0 / 114.0) * (timeTaken - 60) * (timeTaken - 60) + 100); // Same thing. The formula can be calculated by solving y = a(x-60)^2 + b, but I don't bother :/
+                        finalScore =  (float)(-(1.0 / 288.0) * (timeTaken - 60) * (timeTaken - 60) + 100); // Same thing. The formula can be calculated by solving y = a(x-60)^2 + b, but I don't bother :/
                     else
-                        return 0.0f;
+                        finalScore =  50.0f;
 
                 //Hard Difficulty
                 case 2:
                     if(timeTaken < 180)
-                        return 100f;
+                        finalScore =  100f;
                     else if(timeTaken < 300)
-                        return (float)(-(1.0 / 144.0) * (timeTaken - 180) * (timeTaken - 180) + 100); // Ok, I did some math, a = 100 - (300 - 180)^2, b = 100 always
+                        finalScore =  (float)(-(1.0 / 288.0) * (timeTaken - 180) * (timeTaken - 180) + 100); // Ok, I did some math, a = 100 / (300 - 180)^2, b = 100 always
                     else
-                        return 0.0f;
+                        finalScore =  50.0f;
 
                 //Whatever this difficulty is
                 default:
                     System.out.println("You chose a non-existing difficulty and there is no score can measure your greatness...");
-                    return -1.0f;
+                    finalScore =  -1.0f;
             }
+            // If the player goals a score that is lower than their mineFlagged, score = mineFlagged
+            return finalScore;
         }
-        else // Award player the number of flagged mine if they lose
-            return (float)mineFlagged;
+        else {
+            // Award player the number of flagged mine if they lose
+            int difficultyModifier; 
+            switch (difficulty) {
+                case 0:
+                    difficultyModifier = MineSweeperConstants.EASY_MINE_NUM;
+                    break;
+                case 1:
+                    difficultyModifier = MineSweeperConstants.NORMAL_MINE_NUM;
+                    break;
+                case 2:
+                    difficultyModifier = MineSweeperConstants.HARD_MINE_NUM;
+                    break;
+                default:
+                    difficultyModifier = 180;
+                    break;
+            }
+            return (float)mineFlagged / (float)difficultyModifier;
+        }
     }
  }
